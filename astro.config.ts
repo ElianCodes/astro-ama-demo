af7fd02c3aa1b3fe90f51bcb90a8b245bd384e5f
Elian Van Cutsem <hello@elian.codes>
2024-09-20 09:22:17 +0200

    refactor: styling

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,3 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
@@ -6,9 +5,8 @@ import db from '@astrojs/db';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: netlify(),
   integrations: [db(), tailwind(), solidJs()]
 });
